e1315b223fe59adb00d027c0fb2fd5ed1cf8ef9f
Andrea Stagi <stagi.andrea@gmail.com>
2013-09-04 14:05:15 +0200

    Added command line parameters

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A simple script to upload a given .apk or .ipa to testflight.
 
 Usage
 -----
-Create your config.json file in your working directory
+Create your config file in your working directory using json format
 ```
 {
     "api_token": "0000F4K30000",
@@ -15,8 +15,11 @@ Create your config.json file in your working directory
 ```
 Then launch
 ```
-$ ./autoflight.py my_apk_or_ipa_path
+$ ./autoflight.py my_apk_or_ipa_path --config-file config.json
 ```
-License
+Instead of using a config file, you can specify your parameters from the command line, launch the helper to see all the available options
+```
+$ ./autoflight.py --help
+```
 -------
 Relased under MIT license, Copyright (c) 2013 Andrea Stagi <stagi.andrea@gmail.com>
--- autoflight.py
+++ autoflight.py
@@ -9,36 +9,56 @@
 # -------------------------------------------------------------------------
 
 import requests, json, sys, os.path
+from optparse import OptionParser
 
 url = 'http://testflightapp.com/api/builds.json'
 
-print ""
-print "+++++++++++++++++++++++++++"
-print "+      =.4UTOFLIGHT=      +"
-print "+++++++++++++++++++++++++++"
-print ""
+def is_empty(text):
+    return text == None or text == ""
 
-if len(sys.argv) == 1:
+parser = OptionParser()
+parser.add_option( "-a", "--api-token", dest="api_token", default="", help="The API token from the testflight website." )
+parser.add_option( "-t", "--team-token", dest="team_token", default="",help="The team token from the testflight website." )
+parser.add_option( "-d", "--distribution-lists", dest="distribution_lists", default="",help="The name of the distribution list in testflight which should have access to this build." )
+parser.add_option( "-n", "--notify", dest="notify", default=False, help="True if the distribution list should be notified for new builds." )
+parser.add_option( "-o", "--notes", dest="notes", default="", help="Your notes for this build." )
+parser.add_option( "-c", "--config-file", dest="config_file", default="", help="Configuration file" )
+
+(options, args) = parser.parse_args()
+
+if len(args) == 0:
     print "Usage: ./autoflight.py my_apk_or_ipa_path"
     exit(0)
-if len(sys.argv) == 2:
-    build_file = sys.argv[1]
+if len(args) == 1:
+    build_file = args[0]
 
 if not os.path.exists(build_file):
     print "Error! %s file doesn't exist" % build_file
     exit(0)
 config_file = "config.json"
 
-print "Reading configuration..."
-if os.path.exists(config_file):
-    json_file_content = open(config_file, "r").read()
+params = {}
+if not is_empty(options.config_file):
+    print "Reading configuration..."
+    if os.path.exists(options.config_file):
+        json_file_content = open(config_file, "r").read()
+    else:
+        print "Error! config.json file doesn't exist"
+        exit(0)
+    params = json.loads(json_file_content)
 else:
-    print "Error! config.json file doesn't exist"
-    exit(0)
-params = json.loads(json_file_content)
-if not 'notes' in params:
+    params['api_token'] = options.api_token
+    params['team_token'] = options.team_token
+    params['notify'] = options.notify
+    params['notes'] = options.notes
+    params['distribution_lists'] = options.distribution_lists
+
+if not 'notes' in params or is_empty(params['notes']):
     params['notes'] = raw_input("Release Notes - What's new in this build? \n> ")
+
+print params
+
 print "Uploading file..."
 files = {'file': open(build_file, 'rb')}
-req = requests.post(url=url, data=params, files=files)
-print req.text
+#req = requests.post(url=url, data=params, files=files)
+#print req.text
