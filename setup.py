0fd9229a7c2ce3c9f5eb4558b98fdf06ee636688
Andrea Stagi <stagi.andrea@gmail.com>
2013-11-25 00:45:39 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="autoflight",
+      py_modules=['autoflight'],
+      version="1.0",
+      description="A simple script to upload a given .apk or .ipa to testflight.",
+      license="MIT",
+      author="Andrea Stagi",
+      author_email="stagi.andrea@gmail.com",
+      url="https://github.com/atooma/autoflight",
+      keywords= "testflight app ios android test apk ipa script",
+      install_requires=[
+        "requests",
+      ],
+      entry_points = {
+        'console_scripts': [
+            'autoflight = autoflight.autoflight:main',
+        ],
+      },
+      zip_safe = True)
